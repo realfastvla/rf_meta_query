95d41fdab001bcc4f412a5d4a196ac86fa2cd3c8
profxj <xavier@ucolick.org>
2018-11-29 15:11:42 -0800

    added FIRSTS

--- rf_meta_query/radio.py
+++ rf_meta_query/radio.py
@@ -7,6 +7,45 @@ from astropy import units
 from rf_meta_query import catalog_utils
 from rf_meta_query import meta_io
 
+def query_first(frbc, radius=1*units.arcmin, write_meta=False, verbose=False, meta_dir=None):
+    survey = 'FIRST'
+    summary_list = []
+
+    # Meta dir
+    if meta_dir is None:
+        meta_dir = meta_io.meta_dir(frbc)
+
+    # Query
+    first_catalog = catalog_utils.query_hearsarc(frbc, 'first', radius)
+    if first_catalog is None:
+        summary_list += ["{}: There are no sources found within {}".format(survey, radius)]
+        return first_catalog, summary_list
+
+    # Meta -- FIRST specific
+    first_catalog.meta['survey'] = 'FIRST'
+    first_catalog.meta['radius'] = radius.to('arcmin').value
+    # Clean
+    catalog_utils.clean_heasarc(first_catalog)
+    first_catalog['FLUX_20_CM'].unit = units.mJy
+    first_catalog['FLUX_20_CM_ERROR'].unit = units.mJy
+
+    first_catalog.meta['photom'] = 'FLUX_20_CM'
+    first_catalog.meta['photom_mag'] = False
+
+    # Summarize
+    first_summary = catalog_utils.summarize_catalog(frbc, first_catalog, 1*units.arcmin, 'FLUX_20_CM', magnitude=False)
+    summary_list += [first_summary]
+
+    # Sort by coord
+    first_catalog = catalog_utils.sort_by_separation(first_catalog, frbc['coord'], radec=('ra', 'dec'))
+
+    # Write?
+    if write_meta:
+        meta_io.write_catalog(first_catalog, meta_dir, verbose=verbose)
+
+    # Return
+    return first_catalog, first_summary
+
 def query_nvss(frbc, radius=1*units.arcmin, write_meta=False, verbose=False, meta_dir=None):
     survey = 'NVSS'
     summary_list = []
